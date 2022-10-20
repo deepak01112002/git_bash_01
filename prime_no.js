e4cbbc97fc80d0da89b9f117ac9bd243a7772ad3
ritesh22201 <riteshgoswami22201@gmail.com>
2022-10-20 13:35:44 +0530

    Complete code is added

--- prime_no.js
+++ prime_no.js
@@ -1,8 +1,13 @@
 let num = 3;
 let count = 0;
-for(let i = 0; i<=num; i++){
+for(let i = 1; i<=num; i++){
 
    if(num%i==0){
       count++;
    }
-}
+} if(count==2){
+    console.log("It is a Prime Number");
+  }
+  else{
+    console.log("Not a Prime Number");
+  } 
